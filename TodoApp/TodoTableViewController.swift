4cba4e9f76299a5ee23cc96b85d23ce54804d786
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-20 14:39:51 +0900

    UIViewController を継承した TodoTableViewController を追加した

---
+++ TodoApp/TodoTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodoTableViewController.swift
+//  TodoApp
+//
+//  Created by usr0600187 on 2014/11/20.
+//  Copyright (c) 2014年 ai.tashiro. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
