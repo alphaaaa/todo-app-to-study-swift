67ff06a8ac35e02feed9e2b62f14c26b77e34101
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-21 14:31:04 +0900

    TodoDataManagerを作成し、ToDoのデータ構造を定義した

---
+++ TodoApp/TodoDataManager.swift
@@ -0,0 +1,12 @@
+//
+//  TodoDataManager.swift
+//  TodoApp
+//
+//  Created by usr0600187 on 2014/11/21.
+//  Copyright (c) 2014年 ai.tashiro. All rights reserved.
+//
+
+struct TODO {
+    var title : String
+}
+
