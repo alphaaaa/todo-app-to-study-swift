b3ea579db124a524818bdafafbda989c02b07af6
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-21 14:58:43 +0900

    TODOが正しい書式であるか判定するメソッドを追加

--- TodoApp/TodoDataManager.swift
+++ TodoApp/TodoDataManager.swift
@@ -10,3 +10,17 @@ struct TODO {
     var title : String
 }
 
+class TodoDataManager {
+    var todoList: [TODO]
+    
+    // let data = TodoDataManager() で init() が実行される
+    init() {
+        self.todoList = []
+    }
+    
+    // TODOが正しい書式であるか判定する
+    // クラスメソッドとして定義し、クラス名から直接呼び出すのでインスタンスに依存することなく使える
+    class func validate(todo: TODO!) -> Bool {
+        return todo != nil && todo.title != ""
+    }
+}
