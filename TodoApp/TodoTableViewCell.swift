e6698fd3d0719e3694f90a7eb48527713283993d
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-27 14:38:10 +0900

    ToDoリストのセルとなる TodoTableViewCell を作成

---
+++ TodoApp/TodoTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TodoTableViewCell.swift
+//  TodoApp
+//
+//  Created by usr0600187 on 2014/11/27.
+//  Copyright (c) 2014年 ai.tashiro. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell : UITableViewCell {
+    
+}
