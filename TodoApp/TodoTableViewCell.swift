8ec68495e3a56c07b81289985c5d4624a9c7b2e5
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-27 14:48:26 +0900

    initを作成し、GestureRecognizerによる左右へのスワイプ時のアクションを追加

--- TodoApp/TodoTableViewCell.swift
+++ TodoApp/TodoTableViewCell.swift
@@ -10,4 +10,23 @@ import UIKit
 
 class TodoTableViewCell : UITableViewCell {
     
+    init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        // タップされた時は何もしない
+        self.selectionStyle = .None
+        // 背景を白にする
+        self.contentView.backgroundColor = UIColor.whiteColor()
+        
+        // GestureRecognizerの生成と登録
+        // 右方向へのスワイプを認識し、スワイプが行われると hideDeleteButton が実行される
+        self.contentView.addGestureRecognizer(
+            UISwipeGestureRecognizer(target: self, action: "hideDeleteButton")
+        )
+        // 左方向へのスワイプが行われると showDeleteButton が実行される
+        let swipeRecognizer = UISwipeGestureRecognizer(target: self, action: "showDeleteButton")
+        swipeRecognizer.direction = .Left
+        self.contentView.addGestureRecognizer(swipeRecognizer)
+    }
+    
 }
