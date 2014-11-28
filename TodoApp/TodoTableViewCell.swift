9d5d3d242979725b6354dd509de58c02dd1bb63d
Ai Tashiro <alph_a@paperboy.co.jp>
2014-11-28 14:33:24 +0900

    削除ボタンを閉じるメソッドを追加

--- TodoApp/TodoTableViewCell.swift
+++ TodoApp/TodoTableViewCell.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class TodoTableViewCell : UITableViewCell {
+    // 削除ボタンが表示されている状態かを判定する
     var haveButtonsDisplayd = false
     
     init(style: UITableViewCellStyle, reuseIdentifier: String!) {
@@ -33,13 +34,29 @@ class TodoTableViewCell : UITableViewCell {
     // 削除ボタンの表示
     func showDeleteButton() {
         if !haveButtonsDisplayd {
+            // セルの右側が開くアニメーションを0.1秒間行う
             UIView.animateWithDuration(0.1, animations: {
                 let size = self.contentView.frame.size
                 let origin = self.contentView.frame.origin
                 
                 self.contentView.frame = CGRect(x: origin.x - 100, y: origin.y, width: size.width, height: size.height)
             }) {
+                // 削除ボタンが開いた状態
                 completed in self.haveButtonsDisplayd = true
+            }
+        }
+    }
+    
+    // 削除ボタンを閉じる
+    func hideDeleteButton() {
+        if haveButtonsDisplayd {
+            UIView.animateWithDuration(0.1, animations: {
+                let size = self.contentView.frame.size
+                let origin = self.contentView.frame.origin
+                
+                self.contentView.frame = CGRect(x: origin.x + 100, y: origin.y, width: size.width, height: size.height)
+                }) {
+                    completed in self.haveButtonsDisplayd = false
             }
         }
     }
